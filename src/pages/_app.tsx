0e1a0903a030ba65a3aca350feed6c09dd38a4d2
Jack Pordi <jackel119@gmail.com>
2021-11-14 16:46:23 +0000

    tailwind

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Nav } from '../components/Nav';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Nav />
+      <div className="container max-w-6xl mx-auto px-4 pt-16">
+        <Component
+          {...pageProps} />
+      </div>
+    </div>
+  );
+}
+
+export default MyApp
