03a6350b381e4e63fc97fb63235c838f507880d1
Jack Pordi <jackel119@gmail.com>
2021-11-16 23:41:05 +0000

    wip

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,30 +1,48 @@
 import Link from 'next/link'
+import { FaEnvelope, FaGithub } from "react-icons/fa";
 import { FunctionComponent } from 'react'
 
 export const Nav: FunctionComponent = () => {
   return (
-    <nav className="inset-x-0 fixed flex flex-1 items-center justify-between flex-wrap bg-green-500 p-6 py-22 text-white">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-2xl tracking-tight">Jack Pordi</span>
-      </div>
+    <nav className="inset-x-0 fixed flex flex-1 items-center justify-between flex-wrap p-6 py-22 text-gray-500">
+      <a 
+        href="/"
+        className="flex items-center flex-shrink-0 mr-6"
+      >
+        <span className="text-gray-700 text-2xl tracking-tight">Jack <span className="text-blue-600 font-bold">Pordi</span></span>
+      </a>
       <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        <button className="flex items-center px-3 py-2 border rounded text-black border-teal-400 hover:border-white">
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
         </button>
       </div>
       <div className="w-full lg:flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-2xl lg:flex-grow lg:items-center lg:flex justify-end">
+        <div className="text-xl lg:flex-grow lg:items-center lg:flex justify-end">
           <Link href="/about" passHref>
-            <div className="block mt-4 lg:inline-block lg:mt-0 cursor-pointer hover:text-green-100 mr-4">
+            <div className="block mt-4 lg:inline-block lg:mt-0 cursor-pointer hover:text-black mr-6 transition transform hover:scale-110">
               About
             </div>
           </Link>
           <Link href="/posts" passHref>
-            <div className="block mt-4 lg:inline-block lg:mt-0 cursor-pointer hover:text-green-100 mr-4">
+            <div className="block mt-4 lg:inline-block lg:mt-0 cursor-pointer hover:text-black mr-6 transition transform hover:scale-110">
               Posts
             </div>
           </Link>
-          <a href="mailto: contact@jackpordi.com" className="inline-block px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0">Contact</a>
+          <a
+            className="mr-6 transition hover:text-black transform hover:scale-110"
+            href={"mailto: contact@jackpordi.com"}
+          >
+            <FaEnvelope
+              size={28}
+            />
+          </a>
+          <a
+            className="mr-6 transition hover:text-black transform hover:scale-110"
+            href={"https://github.com/jackel119"}>
+            <FaGithub
+              size={28}
+            />
+          </a>
         </div>
       </div>
     </nav>
