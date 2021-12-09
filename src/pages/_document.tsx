c4e3d7ac2cec57e9b1aabc9c13984f59d702661d
Jack Pordi <jackel119@gmail.com>
2021-12-09 22:29:02 +0000

    Fix viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -49,6 +49,7 @@ class MyDocument extends Document {
             });`
             }}
           />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
         <body>
           <Main />
