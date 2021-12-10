102fc2d7bbfe45434f8355734ff3a329036bfb97
Jack Pordi <jackel119@gmail.com>
2021-12-10 00:24:45 +0000

    Move viewport tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <div className="min-h-screen pb-4">
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -51,7 +51,6 @@ class MyDocument extends Document {
             });`,
             }}
           />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
         <body>
           <Main />
