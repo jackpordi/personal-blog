cc195770ac9f10698050610e857962d032ab95cc
Jack Pordi <jackel119@gmail.com>
2022-02-05 14:49:19 +0000

    Add react tests

---
+++ __tests__/PostPreviewCard.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react'
+import moment from 'moment';
+
+import { FrontMatter } from 'types';
+import { PostPreviewCard } from "components/PostPreviewCard";
+
+describe('PostPreviewCard', () => {
+
+  const POST_ID = "test-post-id";
+  const POST: FrontMatter = {
+    title: "Example Post Title",
+    date: moment().format(),
+    abstract: "This is a sample article abstract",
+    image:  "https://image-url.com",
+  };
+
+  it("Renders the post title", () => {
+    render(<PostPreviewCard
+      id={POST_ID}
+      post={POST}
+    />);
+
+    const title = screen.getByRole('heading', {
+      name: POST.title,
+    })
+
+    expect(title).toBeInTheDocument()
+  });
+
+  it("Renders the post abstract", () => {
+    render(<PostPreviewCard
+      id={POST_ID}
+      post={POST}
+    />);
+
+    const abstract = screen.getByText(POST.abstract);
+    expect(abstract).toBeInTheDocument()
+  });
+
+  it("Renders the post image", () => {
+    render(<PostPreviewCard
+      id={POST_ID}
+      post={POST}
+    />);
+
+    const img = screen.getByAltText(POST.image!);
+    expect(img).toBeInTheDocument()
+  });
+
+  it("Matches the snapshot", () => {
+    const { container } = render(<PostPreviewCard
+      id={POST_ID}
+      post={POST}
+    />);
+    expect(container).toMatchSnapshot()
+  })
+
+})
