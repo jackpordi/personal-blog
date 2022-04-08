713907d8386b62cd1f08cbef43610bd08234a5a3
Jack Pordi <jackel119@gmail.com>
2022-04-08 23:13:18 +0100

    Change gradient again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer
       className="items-center justify-center flex-grow flex py-2 text-gray-600 min-w-full height-12 bottom-0 left-0 right-0"
     >
-      &copy; 2021 Jack Pordi. All rights reserved
+      &copy; 2022 Jack Pordi. All rights reserved
     </footer>
   );
 }
