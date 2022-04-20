c29cfe6ede67e3a018fbc0c397904bd7692ab1b2
Jack Pordi <jackel119@gmail.com>
2022-04-20 22:40:00 +0100

    Add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://jackpordi.com',
+  generateRobotsTxt: true,
+  changefreq: "monthly",
+  autoLastmod: false,
+}
