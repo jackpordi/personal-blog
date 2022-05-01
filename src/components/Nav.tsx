ee97840e563045681a0a670239da237620a4ba9f
Jack Pordi <jackel119@gmail.com>
2022-05-01 11:59:12 +0100

    Fix root nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,16 +6,19 @@ import { DarkModeToggle } from "./DarkModeToggle";
 
 export const Nav: FunctionComponent = () => (
   <nav className="inset-x-0 flex flex-1 items-center justify-between flex-wrap p-6 py-22 text-gray-500">
-    <a
+    <Link
       href="/"
-      className="flex items-center flex-shrink-0 mr-6"
     >
-      <span className="text-gray-700 dark:text-white text-3xl md:text-3xl tracking-tight">
+      <a
+        className="flex items-center flex-shrink-0 mr-6"
+      >
+        <span className="text-gray-700 dark:text-white text-3xl md:text-3xl tracking-tight">
         Jack
-        {" "}
-        <span className="gradient-text dark:text-cyan-500 font-bold">Pordi</span>
-      </span>
-    </a>
+          {" "}
+          <span className="gradient-text font-bold">Pordi</span>
+        </span>
+      </a>
+    </Link>
     <div className="block lg:hidden">
       <button className="flex items-center px-3 py-2 border rounded text-black border-teal-400 hover:border-white">
         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
