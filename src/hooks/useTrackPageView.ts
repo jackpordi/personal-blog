89d93c2487a84520b0cde26208568e8ffb021a6f
Jack Pordi <jackel119@gmail.com>
2022-08-11 14:04:30 +0700

    Lint

--- src/hooks/useTrackPageView.ts
+++ src/hooks/useTrackPageView.ts
@@ -4,16 +4,14 @@ import { useEffect } from "react";
 const GA_ID: string = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!;
 
 function logPageView(url: string) {
-  window.gtag('config', GA_ID, {
-    page_path: url,
-  })
+  window.gtag("config", GA_ID, { page_path: url });
 }
 
 export function useTrackpageView(): void {
   const router = useRouter();
 
   useEffect(() => {
-    router.events.on('routeChangeComplete', logPageView)
-    return () =>  router.events.off('routeChangeComplete', logPageView);
-  }, [router.events])
+    router.events.on("routeChangeComplete", logPageView);
+    return () => router.events.off("routeChangeComplete", logPageView);
+  }, [ router.events ]);
 }
