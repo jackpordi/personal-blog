722ef2845979284cf9bebdcd3ee24ff5902483d6
Jack Pordi <jackel119@gmail.com>
2023-11-21 00:33:10 +0000

    Remove console log

--- next.config.js
+++ next.config.js
@@ -33,6 +33,4 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-const final =  withSentryConfig(moduleExports, sentryWebpackPluginOptions);
-console.log(final);
-module.exports =final
+module.exports =  withSentryConfig(moduleExports, sentryWebpackPluginOptions);
